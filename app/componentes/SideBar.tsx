5a8b7cf7465af2037b08d3ce07f0ec0c2c01cb20
bianunees <seuemail@example.com>
2026-03-04 19:46:21 -0300

    feat: adcionando perfil

--- app/componentes/SideBar.tsx
+++ app/componentes/SideBar.tsx
@@ -1,19 +1,25 @@
 "use client";
 
+import NextLink from "next/link"; 
 import { Home, Clock, Link, MapPin, User } from "lucide-react";
 
 export default function Sidebar() {
   return (
     <div className="w-20 h-screen bg-white shadow-md flex flex-col items-center py-6 gap-6">
       
-      <div className="bg-red-500 p-3 rounded-xl text-white">
-        <Home size={22} />
-      </div>
+      <NextLink href="/">
+        <div className="bg-red-500 p-3 rounded-xl text-white cursor-pointer">
+          <Home size={22} />
+        </div>
+      </NextLink>
 
-      <Clock className="text-gray-400" />
-      <Link className="text-gray-400" />
-      <MapPin className="text-gray-400" />
-      <User className="text-gray-400" />
+      <NextLink href="/historico">
+        <Clock className="text-gray-400 cursor-pointer" />
+      </NextLink>
+
+      <NextLink href="/perfil">
+        <User className="text-gray-400 cursor-pointer" />
+      </NextLink>
       
     </div>
   );
