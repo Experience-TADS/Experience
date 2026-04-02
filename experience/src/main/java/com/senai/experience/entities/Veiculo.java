e882f50a108a67f566324dba0973dcec6282147e
juliaharumi2006 <juharumi.nas@gmail.com>
2026-04-01 22:28:04 -0300

    feat: classe vendedor finalizada issue#41

---
+++ experience/src/main/java/com/senai/experience/entities/Veiculo.java
@@ -0,0 +1,36 @@
+package com.senai.experience.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;   
+import lombok.Getter;
+import lombok.NoArgsConstructor;    
+import lombok.Setter;
+
+@Getter //getter para todos os atributos
+@Setter //setter para todos os atributos
+@AllArgsConstructor //construtor com todos os argumentos  
+@NoArgsConstructor //construtor sem argumentos
+@Entity
+@Table(name = "tb_veiculo")
+public class Veiculo {
+    //ATRIBUTOS
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // use Long for JPA primary key
+
+    private int idProduto;
+    private int chassi;
+    private String StatusVeiculo;
+    
+
+    //TO STRING
+    @Override   
+    public String toString() {
+        return "Veiculo{" +
+                "id=" + id +
+                ", idProduto=" + idProduto +
+                ", chassi=" + chassi +
+                ", StatusVeiculo='" + StatusVeiculo + '\'' +
+                '}';
+    }
+}
