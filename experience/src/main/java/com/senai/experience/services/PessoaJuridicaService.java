ce698b68ae32b8916fabc4e4f3c078beb651b401
paolacostxx <paolacosta599@gmail.com>
2026-04-24 21:31:51 -0300

    fix: encoding de senha no update e cadastros de PF/PJ (card 2)

--- experience/src/main/java/com/senai/experience/services/PessoaJuridicaService.java
+++ experience/src/main/java/com/senai/experience/services/PessoaJuridicaService.java
@@ -1,15 +1,21 @@
 package com.senai.experience.services;
 import com.senai.experience.entities.PessoaJuridica;
 import com.senai.experience.repositories.PessoaJuridicaRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
 
 @Service
 public class PessoaJuridicaService {
-    @Autowired
-    private PessoaJuridicaRepository repository;
+    
+    private final PessoaJuridicaRepository repository;
+    private final PasswordEncoder passwordEncoder;
+
+    public PessoaJuridicaService(PessoaJuridicaRepository repository, PasswordEncoder passwordEncoder){
+        this.repository = repository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     public List<PessoaJuridica> findAll() {
         return repository.findAll();
@@ -20,6 +26,17 @@ public class PessoaJuridicaService {
     }
 
     public PessoaJuridica save(PessoaJuridica pessoaJuridica) {
+       if(pessoaJuridica.getId() !=null && repository.existsById(pessoaJuridica.getId())){
+          PessoaJuridica atual = repository.findById(pessoaJuridica.getId()).get();
+
+          if(!passwordEncoder.matches(pessoaJuridica.getSenhaHash(), atual.getSenhaHash())){
+            pessoaJuridica.setSenhaHash(passwordEncoder.encode(pessoaJuridica.getSenhaHash()));
+          }else{
+            pessoaJuridica.setSenhaHash(atual.getSenhaHash());
+          }
+        } else{
+            pessoaJuridica.setSenhaHash(passwordEncoder.encode(pessoaJuridica.getSenhaHash()));
+       }
         return repository.save(pessoaJuridica);
     }
 
