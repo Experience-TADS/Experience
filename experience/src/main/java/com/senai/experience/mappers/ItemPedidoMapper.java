da9433136cd35be6aa121c0ecafe1fbbc0a7f2a0
vitorh07 <vitor07devsenai@gmail.com>
2026-05-14 20:29:15 -0300

    feat: Enriquecer Pedido Response #214

--- experience/src/main/java/com/senai/experience/mappers/ItemPedidoMapper.java
+++ experience/src/main/java/com/senai/experience/mappers/ItemPedidoMapper.java
@@ -3,16 +3,27 @@ package com.senai.experience.mappers;
 import com.senai.experience.DTO.request.ItemPedidoRequest;
 import com.senai.experience.DTO.response.ItemPedidoResponse;
 import com.senai.experience.entities.ItemPedido;
+import com.senai.experience.entities.Pedido;
 import com.senai.experience.entities.Produto;
 
 public class ItemPedidoMapper {
 
     public static ItemPedido toEntity(ItemPedidoRequest dto) {
         ItemPedido i = new ItemPedido();
-        // ItemPedido usa @ManyToOne Produto, então montamos a referência pelo id
-        Produto produto = new Produto();
-        produto.setIdProduto(dto.getIdProduto());
-        i.setProduto(produto);
+        
+        // Cria referência ao Pedido pelo id
+        if (dto.getIdPedido() != null) {
+            Pedido pedido = new Pedido();
+            pedido.setId(dto.getIdPedido());
+            i.setPedido(pedido);
+        }
+        
+        if (dto.getIdProduto() != null) {
+            Produto produto = new Produto();
+            produto.setIdProduto(dto.getIdProduto());
+            i.setProduto(produto);
+        }
+        
         i.setQuantidade(dto.getQuantidade());
         return i;
     }
@@ -20,11 +31,18 @@ public class ItemPedidoMapper {
     public static ItemPedidoResponse toResponse(ItemPedido i) {
         ItemPedidoResponse r = new ItemPedidoResponse();
         r.setIdItemPedido(i.getIdItemPedido());
-        if (i.getProduto() != null) {
-            r.setIdProduto(i.getProduto().getIdProduto());
-            r.setModeloProduto(i.getProduto().getModelo());
-        }
         r.setQuantidade(i.getQuantidade());
+        
+        if (i.getProduto() != null) {
+            ItemPedidoResponse.ProdutoInfo produtoInfo = new ItemPedidoResponse.ProdutoInfo();
+            produtoInfo.setId(i.getProduto().getIdProduto());
+            produtoInfo.setModelo(i.getProduto().getModelo());
+            produtoInfo.setCor(i.getProduto().getCor());
+            produtoInfo.setVersao(i.getProduto().getVersao());
+            produtoInfo.setAno(i.getProduto().getAno());
+            r.setProduto(produtoInfo);
+        }
+        
         return r;
     }
 }
